c27daf9e62c7e580cb4c0d7a4b4eb0b020785687
Arkadiusz Biel <a.biel@ry.com>
2025-06-07 10:27:02 +0100

    remove all files in wwwroot as they are autogenerated

--- src/Limbo.Umbraco.Tables.StaticAssets/wwwroot/Legacy/Scripts/Controllers/TableDataOverlay.js
+++
@@ -1,8 +0,0 @@
-﻿angular.module("umbraco").controller("Limbo.Umbraco.Tables.Overlay.Controller", function ($scope, formHelper) {
-    $scope.save = function () {
-        if (formHelper.submitForm({ scope: $scope })) {
-            formHelper.resetForm({ scope: $scope });
-            $scope.model.submit($scope.model);
-        }
-    };
-});
