37e1c730f6dbab5e127d6b3d9f2071f3212a7a24
Jamie Attwood <jattwood@proteaninteractive.com>
2026-06-25 10:59:54 -0400

    Nuget Release and Debug Updates
    - Added Limbo.Umbraco.Tables.StaticAssets to the build
    - Note: output paths inherited from previous versions

--- debug.bat
+++ debug.bat
@@ -1,2 +1,3 @@
 @echo off
-dotnet build src/Limbo.Umbraco.Tables --configuration Debug /t:rebuild /t:pack -p:PackageOutputPath=c:\nuget\Umbraco17
+dotnet build src/Limbo.Umbraco.Tables.StaticAssets --configuration Debug /t:rebuild /t:pack -p:PackageOutputPath=C:\nuget\local
+dotnet build src/Limbo.Umbraco.Tables --configuration Debug /t:rebuild /t:pack -p:PackageOutputPath=C:\nuget\local
